6b8b04fa0f2e8fb11f374e2c1124cceaed2b07d4
fabien <fabien.rigaux@free.fr>
2025-02-23 19:21:39 +0100

    Start dev

--- src/main/kotlin/fr/fabien/api/cotations/restcontroller/exception/ClientErrorExceptionHandler.kt
+++ src/main/kotlin/fr/fabien/api/cotations/restcontroller/exception/ClientErrorExceptionHandler.kt
@@ -4,7 +4,6 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 
-
 @RestControllerAdvice
 class ClientErrorExceptionHandler {
     @ExceptionHandler(ClientErrorException::class)
