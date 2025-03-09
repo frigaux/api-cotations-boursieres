c1698300217fe4faed83cefec6d888d751500420
fabien <fabien.rigaux@free.fr>
2025-03-09 18:15:33 +0100

    Start dev

--- launchAPI.sh
+++ launchAPI.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-java -Dspring.profiles.active=dev -jar target/api-cotations-boursieres-0.0.1-SNAPSHOT.jar
+dir=target
+
+java -Dspring.profiles.active=dev -jar $dir/api-cotations-boursieres-0.0.1-SNAPSHOT.jar
