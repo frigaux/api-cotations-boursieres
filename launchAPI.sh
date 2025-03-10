03215334b8ec253108628e956c38c0e0e58e3edc
fabien <fabien.rigaux@free.fr>
2025-03-10 17:48:02 +0100

    Start dev

--- launchAPI.sh
+++ launchAPI.sh
@@ -2,4 +2,6 @@
 
 dir=target
 
-java -Dspring.profiles.active=dev -jar $dir/api-cotations-boursieres-0.0.1-SNAPSHOT.jar
+kill $(ps aux | grep 'spring.profiles.active' | awk '{print $2}')
+
+nohup java -Dspring.profiles.active=dev -jar $dir/api-cotations-boursieres-0.0.1-SNAPSHOT.jar &
