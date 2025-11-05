a4fbb34e553f12d894f5e32cc336b2958f45fcdd
fabien <fabien.rigaux@free.fr>
2025-11-05 16:08:59 +0100

    log de la timezone dans WebMvcConfig

--- src/main/kotlin/fr/fabien/api/cotations/configuration/impl/WebMvcConfig.kt
+++ src/main/kotlin/fr/fabien/api/cotations/configuration/impl/WebMvcConfig.kt
@@ -1,16 +1,19 @@
 package fr.fabien.api.cotations.configuration.impl
 
 import fr.fabien.api.cotations.configuration.impl.cache.CustomCacheControl
+import mu.KotlinLogging
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 import org.springframework.web.servlet.mvc.WebContentInterceptor
+import java.util.Calendar
 
 @Configuration
 class WebMvcConfig(@Value("\${cache.expired.hour}") private val heureCacheExpire: Int) : WebMvcConfigurer {
     override fun addInterceptors(registry: InterceptorRegistry) {
         super.addInterceptors(registry)
+        KotlinLogging.logger {}.info("Timezone : ${Calendar.getInstance().getTimeZone().getDisplayName()}")
         registry.addInterceptor(
             WebContentInterceptor()
                 .apply {
